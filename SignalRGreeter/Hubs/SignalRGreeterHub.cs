2343b244f283a0a8ed4c406e703a7c847baa8599
KinNeko <nils@diekmann-germany.de>
2019-07-04 00:07:29 +0200

    Add project files.

---
+++ SignalRGreeter/Hubs/SignalRGreeterHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRGreeter.Hubs
+{
+	public class SignalRGreeterHub : Hub
+	{
+		public string SayHello(string user)
+		{
+			return "Hello " + user;
+		}
+	}
+}
